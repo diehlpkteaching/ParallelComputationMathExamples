1fc8f39cf43315f2b6ee28b22d68ab45bb589af8
Patrick <patrickdiehl@lsu.edu>
2020-04-23 20:46:35 -0500

    Add example for a switch statements

---
+++ chapter2/lecture1-switch.cpp
@@ -0,0 +1,15 @@
+// Example for a switch statement
+#include <iostream>
+
+int main()
+{
+    enum color {RED, GREEN, BLUE};
+    switch(RED) {
+        case RED:   std::cout << "red\n"; break;
+        case GREEN: std::cout << "green\n"; break;
+        case BLUE:  std::cout << "blue\n"; break;
+    }
+    
+    return 0;
+}
+
