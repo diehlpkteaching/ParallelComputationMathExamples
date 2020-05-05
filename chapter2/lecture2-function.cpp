6754bd94ed829ae2b3490d8870ebd6d4e1d35499
Patrick <patrickdiehl@lsu.edu>
2020-05-05 15:38:53 -0500

    Add example for functions

---
+++ chapter2/lecture2-function.cpp
@@ -0,0 +1,24 @@
+// Example with if statement
+#include <iostream>
+
+size_t f(size_t i)
+{
+	    if(	i % 1 == 0)
+		    return  i;
+	    else
+		    return  i * i;
+}
+
+
+int main()
+{
+    size_t result = 0;
+    for(size_t i = 1; i != 5; i++){
+        result = f(i);
+    }
+
+    std::cout << "Result= " << result << std::endl;
+
+    return 0;
+}
+
