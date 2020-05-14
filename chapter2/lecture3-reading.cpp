e861c9f5d2d70c66bf4c8ce92a504ea018c77d9a
Patrick <patrickdiehl@lsu.edu>
2020-05-13 21:02:36 -0500

    Add reading example

---
+++ chapter2/lecture3-reading.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+  return 0;
+}
